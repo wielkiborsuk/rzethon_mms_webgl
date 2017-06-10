f3f48b6eed14df9d175aff7b51c0f800214c3248
wielki.borsuk <wielki.borsuk@gmail.com>
2017-06-11 00:20:48 +0200

    make message component work - send messages

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptionsArgs, Headers } from "@angular/http";
+
+import { AuthService } from '../auth.service';
+import { StateService } from '../state.service';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  lastMessage = {};
+  nodes = [];
 
-  ngOnInit() {
+  message = {
+    sender: this.auth.getCurrentUser(),
+    receiver: 'ThePilot',
+    destination: 'EARTH#1.3',
+    content: 'Draw me a lamb!'
   }
 
+  constructor(private http: Http, private auth: AuthService, private state: StateService) { }
+
+  ngOnInit() {
+    this.fetchDestinations();
+  }
+
+  fetchDestinations() {
+    this.http.get(this.state.BACKEND_URL + '/nodes').subscribe(res => {
+      this.nodes = res.json().nodes;
+      this.message.destination = this.nodes[0].name;
+    });
+  }
+
+  sendMessage() {
+    this.http.post(this.state.BACKEND_URL + '/messages', {'message': this.message}).subscribe(res => {
+      console.log(res);
+      this.lastMessage = res.json().message;
+      this.http.get(this.state.BACKEND_URL + '/simulations').subscribe(res => {
+        console.log(res);
+      });
+    });
+  }
 }
